c82d5f928fbe1a03d64bebf4f67b80ca0d3e2711
yangwonjoon <yangwj@sellmate.co.kr>
2026-03-27 11:45:19 +0900

    완전 탐색

--- archive/boj/2231/yangwj/분해합.js
+++ archive/boj/2231/yangwj/분해합.js
@@ -5,13 +5,16 @@ const input = fs.readFileSync(filePath).toString().trim().split("\n");
 const number = Number(input[0]);
 let result = 0;
 
+// 1부터 확인
 for (i = 1; i <= number; i++) {
+  // i의 각 자릿수의 합을 구한다 (245 → 2+4+5 = 11)
   const test = i
     .toString()
     .split("")
     .map(Number)
     .reduce((acc, cur) => acc + cur, 0);
 
+  // 각 자릿수 합 + 현재 i숫자가 같은거 처음 찾으면 return
   if (test + i === number) {
     result = i;
     break;
